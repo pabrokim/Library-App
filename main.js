e19f63dad21d537905d7feeef162b51ee1badba0
Paul Kimani <pabrokimani@gmail.com>
2023-06-14 12:14:35 +0300

    Add button that brings up form

--- main.js
+++ main.js
@@ -1,43 +1,66 @@
 let myLibrary = [];
 
-function createBooks() {
-  let title = prompt('the title');
-  let author = prompt('author');
-  function Book(title, author) {
-  this.title = title
-  this.author = author
-  }
-  let book = new Book(title, author);
-  return book;
-  
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    let books = prompt('Enter the number of books');    
-    for(let i =0; i <= books.length; i++) {
-        let book = createBooks();
-        myLibrary.push(book);
-    }
+function createBooks() {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').value;
+  let bookObj = new Book(title, author, pages, read);
+  return bookObj;
 }
+
+
+function addBookToLibrary(event) {
+    event.preventDefault();
+    let book = createBooks();
+    myLibrary.push(book);
+    document.querySelector('#user-form').reset();
+    displayBook();
+}
+
 
 function displayBook(){
-    
-    addBookToLibrary()
-
     let container = document.querySelector('.container');
-    
+    container.innerHTML = '';
+
     for(let i= 0; i <= myLibrary.length; i++) {
-        let items = myLibrary[i];
+
+        let bookItem = myLibrary[i];
         let card = document.createElement('div');
         card.classList.add('card');
+
         let titleElement = document.createElement('p');
-        titleElement.textContent = `title: ${items.title}`;
+        titleElement.textContent = `Title: ${bookItem.title}`;
         let authorElement = document.createElement('p');
-        authorElement.textContent = `author: ${items.author}`;
+        authorElement.textContent = `Author: ${bookItem.author}`;
+        let pageElement = document.createElement('p');
+        pageElement.textContent = `Number of pages: ${bookItem.pages} pages`;
+        let readElement = document.createElement('p');
+        readElement.textContent = `Read: ${bookItem.read}`;
+
         card.appendChild(titleElement);
         card.appendChild(authorElement);
+        card.appendChild(pageElement);
+        card.appendChild(readElement);
         container.appendChild(card);
     }
     
 }
-displayBook();
+ 
+function toggleForm() {
+    let form = document.querySelector('#user-form');
+    form.classList.toggle('hide');
+}
+
+let submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', addBookToLibrary);
+
+let toggleButton = document.querySelector('.toggle');
+toggleButton.addEventListener('click', toggleForm);
